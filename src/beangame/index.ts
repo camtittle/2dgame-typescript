7eefd32e41c38825b9ad9e6a87f16be5e1322ab3
camtittle <camtittle@gmail.com>
2020-06-05 12:37:50 +0100

    Add wecome screen that asks for nickname

--- src/beangame/index.ts
+++ src/beangame/index.ts
@@ -1,5 +1,27 @@
 import {BeanGameClient} from "./BeanGameClient";
 
 const canvasElementId = 'gameCanvas';
-const game = new BeanGameClient(canvasElementId);
-game.boot();
+
+showWelcomeScreen();
+
+function showWelcomeScreen() {
+  const nicknameInput = document.getElementById('nicknameInput') as HTMLInputElement;
+  const canvas = document.getElementById(canvasElementId);
+  document.getElementById('joinButton').onclick = function() {
+    const nickname = nicknameInput.value;
+    if (!nickname) {
+      alert('Please enter a valid nickname');
+      return;
+    } else {
+      nicknameInput.disabled = true;
+      document.getElementById('welcomeScreen').classList.add('hidden');
+      launchGameClient(nickname);
+    }
+  }
+}
+
+function launchGameClient(username: string) {
+  document.getElementById(canvasElementId).classList.remove('hidden');
+  const game = new BeanGameClient(canvasElementId);
+  game.boot();
+}
